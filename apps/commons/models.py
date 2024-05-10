116762753e9ed5ae559e0f2b5a04d10b4caf3eaf
shresthaaman27@gmail.com <shresthaaman27@gmail.com>
2024-05-10 18:17:09 +0545

    models added

---
+++ apps/commons/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+import uuid
+from django.db import models
+
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
---
+++ config/settings/env.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-&z&kz01pzf)#hao!2ic@uk_t#emh8mg#khmj1k!g7c5b_r&$^i'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
